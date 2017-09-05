ff80b1429357bcefb27ead653844cbada4e7c1df
syuan0321 <syuan0321@gmail.com>
2017-09-05 17:33:57 -0400

    utils StringFromConstArray

---
+++ src/main/java/com/syuan0321/unitseed/utils/StringFromConstArray.java
@@ -0,0 +1,31 @@
+package com.syuan0321.unitseed.utils;
+
+public class StringFromConstArray {
+	//Must start with 0, because array 1st position indexed as 0
+	public static final int	TYPE_A= 0;
+	public static final int	TYPE_B= 1;
+	public static final int	TYPE_C= 2;
+	public static final int	TYPE_D= 3;
+	public static final String[] TYPE_VALUES = {"A", "B", "C", "D"};	//Start with index 0
+	
+	private static String fromConstArray(String[] strings, Object index) {
+		int i = 0;
+		if (index instanceof Integer) {
+			i = (Integer)index;
+			if (i < 0 || i >= strings.length) {
+				i = 0;
+			}
+		}
+		return strings[i];
+	}
+	
+	
+	public static void main(String[] args) {
+		for (int i = 0; i <= 3; i++) {
+			String result = fromConstArray(StringFromConstArray.TYPE_VALUES, i);
+			System.out.println(result);		
+		}
+		
+	}
+	
+}
---
+++ src/main/java/com/syuan0321/unitseed/utils/readme.txt
@@ -0,0 +1,22 @@
+/**********************************************************
+This UTILs folder should include all util classes (lol)
+--------------------------------------------------------
+1.StringFromConstArray
+String from Constant Array (start with 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+***********************************************************/
